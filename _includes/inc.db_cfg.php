56a28d21952b5c831350bc43947789d4dc2e1c70
Sammitch <fubes2000@gmail.com>
2013-12-13 14:02:35 -0800

    fixed untracked files.

--- _includes/inc.db_cfg.php
+++
@@ -1,8 +0,0 @@
-<?php
-$database_credentials = array(
-        'hostname'      => 'mysql.hostname.com',
-        'dbname'        => 'dname',
-        'username'      => 'username',
-        'password'      => 'password',
-        'prefix'        => 'pre_'
-);
