64f84419fdb4459f835b1c188c7b1309eb83867f
Sammitch <fubes2000@gmail.com>
2013-12-13 14:07:49 -0800

    re-add skeleton db config

---
+++ _includes/inc.db_cfg.php
@@ -0,0 +1,7 @@
+<?php
+$database_credentials = array(
+        'hostname'      => 'mysql.host.com',
+        'dbname'        => 'db_name',
+        'username'      => 'user_name',
+        'password'      => 'p@ssw0rd'
+);
